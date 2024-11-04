680cdd880495bfbfbd1b84efa7cdc26a99d383fd
Zara <thezarazia@gmail.com>
2024-11-04 08:48:48 +0000

    Add support for commandline arguments
    This commit gives the user the option to supply commandline arguments
    instead of running the script interactively, so that if they like to
    check a particular route frequently, they can automate it.
    
    This commit also includes some misc. cleanups: exception-handling,
    and some usage information.

--- showtrains.py
+++ showtrains.py
@@ -24,6 +24,7 @@
 # This script lets you see what trains are leaving from some station,
 # to some other station. You'd think it'd be very simple, but.
 
+import argparse
 from zeep import Client, Settings, xsd
 from zeep.plugins import HistoryPlugin
 
@@ -51,52 +52,72 @@ header = xsd.Element(
 )
 header_value = header(TokenValue=LDB_TOKEN)
 
-# TODO: commandline --h flag that lists 3-letter codes mapped to
-# station names.
+# We add --start and --dest as optional commandline args, so the script can
+# easily be called by a shellscript or similar for frequent journeys
+parser = argparse.ArgumentParser()
+parser.add_argument("-s", "--start",
+                    help="departure station (three-letter code)")
+parser.add_argument("-d", "--dest",
+                    help="arrival station (three-letter code)")
+parser.add_argument("-r", "-rev", "--reverse",
+                    help="also display reverse journey", action="store_true")
+args = parser.parse_args()
 
-print("What station are you going from? (Input 3 letter code)")
-start = input()
-
-# TODO: Let user list multiple destinations at once
-print("What station are you going to? (Input 3 letter code)")
-dest = input()
-
+# TODO: commandline --l flag that lists 3-letter codes mapped to
+# station names. Apparently there are about 2500 stations in the UK, so the
+# implementation will require a bit of thought. :)
 
 def list_trains(my_start, my_dest):
-    station = my_start
+    my_start = my_start.upper()
+    my_dest = my_dest.upper()
     # GetDepBoardWithDetails is limited to listing 10 departures.
     # But we have to use it, because it's the endpoint that includes stops
     # for each train. In contrast, GetDeparturesBoard retrieves unlimited
     # departures, but no stops, only the 'destination'.
     # This refers to the final stop on the train's route.
     # A hacky workaround (to be implemented) is to use time offsets to get
-    # more results; you can offset by up to 2 hours. That's still not very
-    # much, though for smaller stations it may be sufficient.
+    # more results; you can offset by up to 2 hours, with timeOffset=119.
+    # That's still not very  much,
+    # though for smaller stations it may be sufficient.
     # Hopefully there's some way around this limitation...
-    res = client.service.GetDepBoardWithDetails(numRows=10, crs=station, _soapheaders=[header_value])
+    res = client.service.GetDepBoardWithDetails(numRows=10, crs=my_start, _soapheaders=[header_value])
 
-    print("Trains at " + res.locationName)
+    print("\n Trains from " + res.locationName)
     print("===============================")
 
     trainServices= res.trainServices
-    print("For station: " + station + " to: " + my_dest)
-    for train in trainServices["service"]:
-        lists_of_stops = train.subsequentCallingPoints['callingPointList']
-        for list_of_stops in lists_of_stops:
-            # this is the actual list of stops, per train
-            for stop in list_of_stops['callingPoint']:
-                stop_code = (stop['crs'])
-                if stop_code == my_dest:
-                    if train['isCancelled']:
-                        print("[[Cancelled train hidden from list.]]\n")
-                    else:
-                        print("\n")
-                        print("Train leaves at " + train['std'])
-                        print("Train leaves from platform " + str(train['platform']))
-                        print("Train arrives at " + my_dest + " at " + stop['st'])
-                        print("\n")
+    print(my_start + " to " + my_dest)
+    try:
+        for train in trainServices["service"]:
+            lists_of_stops = train.subsequentCallingPoints['callingPointList']
+            for list_of_stops in lists_of_stops:
+                # this is the actual list of stops, per train
+                for stop in list_of_stops['callingPoint']:
+                    stop_code = (stop['crs'])
+                    if stop_code == my_dest:
+                        if train['isCancelled']:
+                            print("\n [[Cancelled train hidden from list.]]\n")
+                        else:
+                            print("\n Train leaves " + my_start + " at " + train['std'])
+                            print("Train leaves from platform " + str(train['platform']))
+                            print("Train arrives at " + my_dest + " at " + stop['st'])
+    except TypeError:
+        print("NO TRAINS.")
+    print("\n")
 
-list_trains(start, dest)
-print("Reverse journey? (y/n)")
-if input() == "y":
-    list_trains(dest, start)
+# Check for commandline arguments; if none, ask user for input interactively
+if args.start and args.dest != None:
+    list_trains(args.start, args.dest)
+    if args.reverse:
+        list_trains(args.dest, args.start)
+else:
+    print("What station are you going from? (Input 3 letter code)")
+    start = input()
+
+    # TODO: Let user list multiple destinations at once
+    print("What station are you going to? (Input 3 letter code)")
+    dest = input()
+    list_trains(start, dest)
+    print("Reverse journey? (y/n)")
+    if input() == "y" or "Y" or "yes" or "YES":
+        list_trains(dest, start)
